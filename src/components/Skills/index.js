1dd7aa83b7278709903951b54f033c6268b860d0
Kevin Dave Gerona <kgerona@dnamicro.com>
2021-05-14 07:03:41 +0800

    Change: on skills section

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,34 +1,46 @@
-import React from 'react'
+import React from 'react';
 
 // import * as techs from '../../assets/images/technology-logo/*.png'
 
-import {
-    SkillsContainer,
-    HeadingText,
-    LogoContainer,
-    LogoImg,
-} from './styles'
+import { SkillsContainer, HeadingText, LogoContainer, LogoImg } from './styles';
 
-const importAll = (r) => r.keys().map(filename => ({
+const importAll = (r) =>
+  r.keys().map((filename) => ({
     filename,
-    static_path: r(filename).default
-}));
+    static_path: r(filename).default,
+  }));
 
-const images = importAll(require.context('../../assets/images/technology-logo', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(
+  require.context(
+    '../../assets/images/technology-logo',
+    false,
+    /\.(png|jpe?g|svg)$/
+  )
+);
 
-console.log(images[0])
+console.log(images[0]);
 
 const Skills = () => {
-    return (
-        <SkillsContainer id='skills'>
-            <HeadingText>Skills and technologies used ...</HeadingText>
-            <LogoContainer>
-                {
-                    images.map(image => (<LogoImg src={image.static_path} alt={image.filename}/>))
-                }
-            </LogoContainer>
-        </SkillsContainer>
-    )
-}
+  return (
+    <SkillsContainer id='skills'>
+      <HeadingText>Skills and technologies used ...</HeadingText>
+      <LogoContainer>
+        {images.map((image) => (
+          <div
+            style={{
+              width: '100px',
+              height: '100px',
+              display: 'flex',
+              alignItems: 'center',
+              margin: '12px',
+            }}
+          >
+            <LogoImg src={image.static_path} alt={image.filename} />
+          </div>
+        ))}
+      </LogoContainer>
+    </SkillsContainer>
+  );
+};
 
-export default Skills
+export default Skills;
